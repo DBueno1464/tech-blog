f1a0ec417d2d83cf92aab5a38dc9884adc3157aa
DBueno1464 <DerrickBueno00@gmail.com>
2021-09-30 19:23:13 -0400

    added posts models and seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
+const postData = require('./posts.json');
 
 
 const seedDatabase = async () => {
@@ -11,6 +12,13 @@ const seedDatabase = async () => {
         individualHooks: true,
         returning: true,
     });
+
+    for (const posts of postData) {
+        await Post.create({
+            ...posts,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
 
     console.log("-----DATA HAS BEEN SEEDED!-----")
 
