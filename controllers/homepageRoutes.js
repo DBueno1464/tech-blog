b1960da56521b34406387f2ab40ad555da7a6bcc
DBueno1464 <DerrickBueno00@gmail.com>
2021-10-10 00:25:12 -0400

    created homepage, login and sahsboard renders and handlebars

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -31,4 +31,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //     res.redirect('/profile');
+    //     return;
+    // }
+
+    res.render('login');
+});
+
+router.get('/dashboard', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //     res.redirect('/profile');
+    //     return;
+    // }
+
+    res.render('dashboard');
+});
+
 module.exports = router;
